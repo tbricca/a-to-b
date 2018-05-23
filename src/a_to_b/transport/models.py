ac9d0414ab207285c41ad72e999cb23175ba540a
t101jv <t101jv@gmail.com>
2018-05-22 20:56:45 -0700

    Adding city, company and transport models and setup superuser

--- src/a_to_b/transport/models.py
+++ src/a_to_b/transport/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+class Transport(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    link = models.URLField()
+    start = models.ForeignKey(City, related_name='start', on_delete=models.PROTECT)
+    end = models.ForeignKey(City, on_delete=models.PROTECT)
