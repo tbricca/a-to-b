5dc2c34a8eb9c1c4c0b533df499e8fdaaa801453
t101jv <t101jv@gmail.com>
2018-06-22 20:32:44 -0700

    integrating static assets and index template

--- src/a_to_b/a_to_b/settings.py
+++ src/a_to_b/a_to_b/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'transport.apps.TransportConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- src/a_to_b/transport/views.py
+++ src/a_to_b/transport/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('transport/index.html')
+    return HttpResponse(template.render({}, request))
 
 def search(request):
     return HttpResponse("Hello, World!")
